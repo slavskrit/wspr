6e0e2d688fc36dbd43cba98275abba9bf0f44319
slavskrit <dpronin@yandex.ru>
2024-01-27 23:49:03 -0800

    Working but 10gb lol

--- README.md
+++ README.md
@@ -5,3 +5,18 @@ To run please execute:
 ```shell
 docker run -e TELOXIDE_TOKEN=YOUR_TG_TOKEN -e RUST_BACKTRACE=1 wspr
 ```
+
+```shell
+docker build -t wspr .
+```
+
+```shell
+docker save -o wspr.tar wspr:latest
+```
+
+## On device
+
+```shell
+docker load -i wspr.tar
+```
+
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ use teloxide::{dispatching::dialogue::InMemStorage, net::Download, prelude::*};
 use tokio::fs;
 
 type HandlerResult = Result<(), Box<dyn std::error::Error + Send + Sync>>;
-const ARGS: &'static str = "--compute_type int8 --diarize --highlight_words True";
 
 #[derive(Clone, Default)]
 pub enum State {
@@ -14,7 +13,7 @@ pub enum State {
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
-    log::info!("Starting dialogue bot...");
+    log::info!("Starting text to speech bot...");
 
     let bot = Bot::from_env();
 
@@ -31,18 +30,25 @@ async fn main() {
     .await;
 }
 
-fn transcode(path: &str) -> String {
-    let output = Command::new("whisper").args([path, ARGS]).output();
+async fn transcode(path: &str) -> String {
+    let output = Command::new("whisper")
+        .args([path, "--output_format", "txt", "--output_dir", "/tmp"])
+        .current_dir("/tmp")
+        .status();
+
+    println!("a1");
 
     match output {
-        Ok(output) => {
-            if let Ok(stdout) = std::str::from_utf8(&output.stdout) {
-                return String::from(stdout);
-            } else {
-                log::error!("Counld not convert output of the audio: {path}");
+        Ok(_output) => match fs::read_to_string(format!("{path}.txt")).await {
+            Ok(result) => {
+                log::info!("Result for {path}: {result}");
+                return result;
+            }
+            Err(_) => {
+                log::error!("Counld not parse an audio by given path: {path}");
                 return String::new();
             }
-        }
+        },
         Err(_) => {
             log::error!("Counld not parse an audio by given path: {path}");
             return String::new();
@@ -53,23 +59,23 @@ fn transcode(path: &str) -> String {
 async fn start(bot: Bot, msg: Message) -> HandlerResult {
     match msg.voice() {
         Some(p) => {
-            bot.send_message(msg.chat.id, "Started processing").await?;
+            let initial_response = bot
+                .send_message(msg.chat.id, "Started processing 💪")
+                .reply_to_message_id(msg.id)
+                .await
+                .unwrap();
             let file_id = &p.file.id;
             let file = bot.get_file(file_id).await?;
             let path = format!("/tmp/{file_id}");
             let mut dst = fs::File::create(&path).await?;
             bot.download_file(&file.path, &mut dst).await?;
-            let mut output = transcode(&path);
+            let mut output = transcode(&path).await;
             if output.is_empty() {
                 output = "Could not parse a voice message".to_string();
-            } else {
-                output = output
-                    .split("Skipping /tmp/")
-                    .next()
-                    .unwrap_or("")
-                    .to_string()
             }
-            bot.send_message(msg.chat.id, output).await?;
+            let _ = bot
+                .edit_message_text(msg.chat.id, initial_response.id, output)
+                .await;
         }
         None => {
             bot.send_message(msg.chat.id, "Please send a voice memo")
